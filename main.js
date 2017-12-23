b97174d5e81436202d3c4d2d253469efb4169e1d
crveniOrao <crveni_orao@live.com>
2017-12-23 13:09:34 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,72 @@
+var temp = document.getElementById("temp");
+var city = document.getElementById("city")
+var icon = document.getElementById("weatherIcon");
+var desc = document.getElementById("weatherDesc");
+var btn = document.getElementById("refreshBtn");
+var input = document.getElementById("input");
+var dataList = document.getElementById("datalist-json");
+var weatherLocation = "nis";
+
+
+
+
+
+
+window.addEventListener("load", getWeather);
+input.addEventListener("keypress", function (event){
+    
+     if (event.keyCode === 13){
+         event.preventDefault();
+         weatherLocation = this.value;
+         getWeather();
+         this.value = "";
+     }
+ });
+ btn.addEventListener("click", getWeather);
+
+function getWeather(){
+    var url = "http://api.openweathermap.org/data/2.5/weather?q="+weatherLocation+"&units=metric&APPID=07e1a1062609d298c2ca81fcc314cf28";
+    fetch(url)
+    .then(function(request){
+        if(!request.ok){
+            throw Error(request.status);
+        } 
+        return request;
+    })
+    .then(function(res){
+        return res.json()
+        .then(function(data){
+            city.innerHTML = data.name;
+            temp.innerHTML = data.main.temp + "°C";
+            desc.innerHTML = data.weather[0].description;
+            icon.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        })
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+// should be suggestions, but doesn't work well
+/* var cities = [];
+
+$.get( "popularCities.json", function( data ) {
+    $.each(data, function(index, value){
+        var cityName = value.name;
+        cities.push(cityName);
+    });
+});
+
+$("#input").autocomplete({
+    source: cities
+}); */
+
+
+
+
+
+
+
+
+       
+
